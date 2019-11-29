a7c5799d5d55480d8fae6af550b86714d9260ce1
Ruben Jungius <ruben.jungius@epfl.ch>
2019-11-29 10:45:35 +0100

    First Version of Inputreating
    + Added batch file to purge all files created by code
    + Added config file 
    + main.py now initializes values from config file, reads one line from Arduino and saves it in a file. Hello World!

--- GUI/main.py
+++ GUI/main.py
@@ -1,10 +1,40 @@
 # Author: Ruben Jungius
 # Project: IO - GUI Groundstation
-# Organization: EPFL Rocket Team
+# Organization: EPFL Rocket Team, Lausanne
 
 import serial
+import sys
 
-s = serial.Serial('COM5')
-while True:
-    data = s.read()
-    print(data)
+try:
+    config = open('config.txt', 'r')
+except Exception as e:
+    print(e)
+    sys.exit()
+
+for line in config:
+    if line.strip().startswith('#') or line.strip().startswith('\n'):
+        continue
+    if line.strip().startswith('Serial Port:'):
+        port = line.replace('Serial Port:', '').replace('\n', '').strip()
+        try:
+            s = serial.Serial('COM5')
+            print('[Serial] Connection on Port ' + port)
+        except Exception as e:
+            print(e)
+            sys.exit()
+    if line.strip().startswith('Excel Path:'):
+        path = line.replace('Excel Path:', '').replace('\n', '').strip()
+        if path != '' and not path.endswith('\\'):
+            path = path + '\\'
+        try:
+            outfile = open(path + 'data.txt', 'w+')
+            print('[Excel] Sucess creating Excel file '
+                  + 'in current directory' if path == '' else
+                  '[Excel] Sucess creating Excel file at ' + path)
+        except Exception as e:
+            print(e)
+            sys.exit()
+
+data = s.readline()
+outfile.write(data.decode('utf-8').replace('\n', ''))
+outfile.close()
